b5e5e388d73df87eb2852469cfd67ece3165437e
diegobrenes <diego.brenes@icloud.com>
2018-08-31 22:54:11 -0400

    Commiting ProggramingAssisgnment2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,38 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This file contains two functions: makeCacheMAtrix and cacheSolve
 
-## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
+## makeCacheMArix creates a matrix object that caches its inverse
 
+
+makeCacheMatrix <- function(x) {
+
+        im <- NULL
+        set <- function(y){
+                x <<- y
+                im <-- NULL
+        }
+        get <- function() x
+        setinversematrix <- function(solve) im <<- solve
+        getinversematrix <- function() im
+        list(set = set, get = get,
+             setinversematrix = setinversematrix,
+             getinversematrix = getinversematrix)
 }
 
+## cacheSolve requires an argument that is returned by makeCacheMatrix,
+## if cached, retreves the inverse, and if not, executes solve() to return
+## the inverse of the matrix
 
-## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        
+        im <- x$getinversematrix()
+        if(!is.null(im)){
+                message("getting cached data")
+                return(im)
+        }
+        data <- x$get()
+        im <- solve(data, ...)
+        x$setinversematrix(im)
+        im
 }
